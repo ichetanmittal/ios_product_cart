7d02f8ea2a35c0e7a2f4717fdb2a2e9e91744d01
Chetan Mittal <ichetanmittal@gmail.com>
2025-01-01 14:32:27 +0530

    Initial Commit

---
+++ Swipeios/SwipeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeiosApp.swift
+//  Swipeios
+//
+//  Created by Chetan Mittal on 2025/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwipeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwipeiosTests/SwipeiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeiosTests.swift
+//  SwipeiosTests
+//
+//  Created by Chetan Mittal on 2025/01/01.
+//
+
+import Testing
+@testable import Swipeios
+
+struct SwipeiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
