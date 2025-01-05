a5f700469c8e8dee23c8739e080802602bd39402
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-01-06 00:51:04 +0530

    Splash Screen added with Transition into main App

--- Swipeios/SwipeiosApp.swift
+++ Swipeios/SwipeiosApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwipeiosApp: App {
     var body: some Scene {
         WindowGroup {
-            ProductListView()
+            SplashScreenView()
         }
     }
 }
---
+++ Swipeios/Views/SplashScreenView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ProductListView()
+        } else {
+            ZStack {
+                Color.white
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("swipe")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
